2764bad70a7022375e376cd5b20667dc689e6700
James Yarbrough <jjarbrough@gmail.com>
2023-09-28 14:53:08 -0500

    Did the first assignment

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"{name}'s {age} birthday.");
+            Console.WriteLine($"On {name}'s Birhtday they went to the zoo.");
+            Console.WriteLine($"They really wanted to see the {animal}.");
+            Console.WriteLine($"To {name}'s surprise there were baby {animal}s.");
+            Console.WriteLine($"{name} made sure they wore their favorite {color} shirt.");
+            Console.WriteLine($"{band} was playing on the ride home.");
         }
     }
 }
